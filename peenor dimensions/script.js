93610cac49409d2f606c0259c167c0dbbb0c9030
NGCMan <nintendogamecubeman@gmail.com>
2025-10-23 23:10:37 -0700

    Add files via upload

---
+++ peenor dimensions/script.js
@@ -0,0 +1,10 @@
+/* If you're feeling fancy you can add interactivity 
+    to your site with Javascript */
+
+// prints "hi" in the browser's dev tools console
+function myFunction() {
+  location.replace("https://www.youtube.com/watch?v=BT9h5ifR1tY")
+}
+window.onload = function() {
+  myFunction();
+};
